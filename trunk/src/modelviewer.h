1fcb464a940f6acd9b0ecda688c740ed09b72a78
sephiroth3d <sephiroth3d@56fab96a-fa2e-11dd-a7be-4b3ef3b0700c>
2010-01-07 01:26:15 +0000

    Fixed some LW Export associations I forgot about in my last patch, when using the directory options.

--- trunk/src/modelviewer.h
+++ trunk/src/modelviewer.h
@@ -48,7 +48,7 @@
 
 // defines
 #define APP_TITLE _T("World of Warcraft Model Viewer")
-#define APP_VERSION _T("v0.5.08 (Chuanhsing Update r675)")
+#define APP_VERSION _T("v0.5.08 (Chuanhsing Update r676)")
 
 
 class ModelViewer: public wxFrame
