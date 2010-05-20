8c2d97963b84e9914662aeb2ba2bdf7c6d6ccae3
chuanhsing <chuanhsing@56fab96a-fa2e-11dd-a7be-4b3ef3b0700c>
2010-05-20 04:18:35 +0000

    - mpq.h reads defined to switch libmpq or stormlib

--- src/mpq.h
+++ src/mpq.h
@@ -1,14 +1,12 @@
+
 // libmpq based implementation by :wumpus:
-#if defined (_MAC)
-#include "mpq_libmpq.h"
-#elif defined (_LINUX)
-#include "mpq_libmpq.h"
-#endif
-
-// ShadowF mpq library
-//#include "mpq_sfmpq.h"
-
 // StormLib library
 #if defined (_WINDOWS)
 #include "mpq_stormlib.h"
+#else
+	#if defined (USE_STORM)
+	#include "mpq_stormlib.h"
+	#else
+	#include "mpq_libmpq.h"
+	#endif
 #endif
