9c4042416a994302f5a4cc5a851b824e066e8e39
chuanhsing <chuanhsing@56fab96a-fa2e-11dd-a7be-4b3ef3b0700c>
2010-11-03 01:40:43 +0000

    update stormlib

--- trunk/src/stormlib/src/SBaseFileTable.cpp
+++ trunk/src/stormlib/src/SBaseFileTable.cpp
@@ -557,7 +557,7 @@ static TMPQExtTable * LoadExtTable(
 
         // Decrypt the block
         BSWAP_ARRAY32_UNSIGNED(pExtTable + 1, pExtTable->dwDataSize);
-        DecryptMpqBlock(pExtTable + 1, pExtTable->dwDataSize, dwKey);
+        DecryptMpqBlock(pExtTable + 1, Size - sizeof(TMPQExtTable), dwKey);
         BSWAP_ARRAY32_UNSIGNED(pExtTable + 1, pExtTable->dwDataSize);
 
         // If the table is compressed, decompress it
