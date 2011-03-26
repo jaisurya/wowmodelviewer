58f3e87c683c1e8de4ad00ca10cdb6c98577b921
sephiroth3d@gmail.com <sephiroth3d@gmail.com@56fab96a-fa2e-11dd-a7be-4b3ef3b0700c>
2011-03-26 21:34:53 +0000

    Release Version Update

--- trunk/src/modelviewer.h
+++ trunk/src/modelviewer.h
@@ -50,7 +50,7 @@
 
 // defines
 #define APP_TITLE wxT("World of Warcraft Model Viewer")
-#define APP_VERSION wxT("v0.7.0.1 r543")			// Revision number should match the revision number on GoogleCode.
+#define APP_VERSION wxT("v0.7.0.1 r544")			// Revision number should match the revision number on GoogleCode.
 #define APP_BUILDNAME wxT("Gnome Punter")		// Fun thing for developers to play with. Should change with each Main Release. (Not counting DEV WORK editions)
 /*
 	--==List of Build Name ideas==--	(Feel free to add!)
