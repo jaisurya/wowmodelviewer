f4de7afaa23568e5fccc94254f3b718d75606dc4
sephiroth3d@gmail.com <sephiroth3d@gmail.com@56fab96a-fa2e-11dd-a7be-4b3ef3b0700c>
2011-08-05 01:16:08 +0000

    WMV 0.8.0.0 -Built and finished the About Window. -Added Icon for Load World of Warcraft. It changes, depending what version of WoW you're going to load. -Added interface for List of WoW Directories. Currently contains only test data. -Fixed Application Icon issue for Windows. -Removed the generated ui_[Filename].h from the sourcecode. It's auto-generated when the UIs are compiled.

--- branches/wmv0800/Source/application.h
+++ branches/wmv0800/Source/application.h
@@ -7,7 +7,15 @@
 
 // Be careful of what you #include here. This header is included with the auto-generated
 // moc_application.cpp file, and can sometimes cause linking errors.
+
+// Globals
+#include "version.h"
 #include "enums.h"
+
+// Other Windows
+#include "ui_About.h"
+
+// Functions, Exporters & Stormlib
 #include "math\math_base.h"
 #include "Exporters\exporters.h"
 #include "StormLib\src\StormLib.h"
@@ -15,7 +23,24 @@
 
 namespace Ui {
     class Main_Window_Viewer;
+	class AboutWindow;
 }
+
+class WindowAbout : public QDialog
+{
+	Q_OBJECT
+
+public:
+    explicit WindowAbout(QWidget *parent = 0);
+	~WindowAbout();
+private slots:
+	void on_CloseButton_clicked() {
+		QDialog::close();
+	}
+
+private:
+	Ui::AboutWindow *ui_About;
+};
 
 class WoWModelViewer : public QMainWindow
 {
@@ -25,6 +50,8 @@ public:
     explicit WoWModelViewer(QWidget *parent = 0);
 	size_t InterfaceMode;
     size_t ViewerInterfaceType;
+	size_t WoWTypeCurrent;
+	size_t WoWTypeNext;
     void UpdateViewerMenu();
 	void createStatusBar();
 	void updateFileList();
@@ -34,8 +61,16 @@ public:
 	bool isWoWLoaded;		// Is there a version of WoW loaded.
 	bool canReloadWoW;		// If isWoWLoaded == true, are we allowed to reload it? Should only be used if the MPQ list is changed, or we change WoW directories.
 
+	// SubWindows
+	WindowAbout Window_About;
+
+public slots:
+	void ChangeLoadButton();
+	void UpdateLoadButton();
+
 private:
     // Groups
+	QActionGroup *WoWDirGroup;
     QActionGroup *EyeGlowGroup;
     QActionGroup *LandscapeQualityGroup;
     QActionGroup *LightTypeGroup;
@@ -49,7 +84,6 @@ private slots:
     void on_actionInitial_Pose_Only_toggled(bool arg1);
 	void on_actionShowCtrl_FileList_toggled(bool arg1);
 
-	void on_rBtn_WoWNotLoaded_clicked();
     void on_rBtn_NoModel_clicked();
     void on_rBtn_IsChar_clicked();
     void on_rBtn_IsNPC_clicked();
@@ -59,7 +93,9 @@ private slots:
 	void on_rBtn_IsItem_clicked();
     void on_rBtn_IsSound_clicked();
 
+	void on_actionLoad_World_of_Wacraft_triggered();
     void on_actionExit_triggered();
+	void on_actionAbout_triggered();
 
 private:
     Ui::Main_Window_Viewer *ui;
